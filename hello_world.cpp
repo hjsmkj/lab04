aab32b0219ed525446c3b51e142e08b9f4d14325
hjsmkj <vitaliapavlovaa@mail.ru>
2024-08-16 16:08:29 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	std::string name;
-	std::cin>>name;
-	std::cout<<"hello, world from "<<name<<std::endl;
+	std::cin>>name;//ввод имени пользователя
+	std::cout<<"hello, world from "<<name<<std::endl;//вывод фразы
 	return 0;
 }
